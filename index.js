2add557bac79e4db016143c9dd89eb89e2d42fe2
zimbora <lucas.ua.eet@gmail.com>
2025-08-26 12:08:06 +0100

    Expose server resources. Changes lib name to lwm2m-gw

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // index.js
-const { startServer } = require('./lib/server');
 
-startServer();
+// server libs
+const bootstrap = require('./server/bootstrap');
+const resourceClient = require('./server/resourceClient');
+const mqttRequestHandler = require('./server/mqttRequestHandler');
+const clientRegistry = require('./server/clientRegistry');
+const sharedEmitter = require('./server/transport/sharedEmitter');
+
+// client libs
+
+
+module.exports = {
+	bootstrap,
+	resourceClient,
+	mqttRequestHandler,
+	clientRegistry,
+	sharedEmitter,
+}
